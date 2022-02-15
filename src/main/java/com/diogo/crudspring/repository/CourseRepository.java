2e763e4bc1ac3a1cb03ab040295775813a4aa2fd
Diogo <anderle88@gmail.com>
2022-02-15 09:54:03 -0300

    Initial commit - Course CRUD Spring + Angular

---
+++ src/main/java/com/diogo/crudspring/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.diogo.crudspring.repository;
+
+import com.diogo.crudspring.model.Course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    
+}
